b9752bb091e5e34c4e38fa7474925cd1fdc5a246
Slawomir_Kaminski <slawek.kami@gmail.com>
2015-11-15 10:36:30 +0100

    dodanie tablicy - zakończenie zadania

--- src/com/slawekkami/Main.java
+++ src/com/slawekkami/Main.java
@@ -3,23 +3,32 @@ package com.slawekkami;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.regex.Pattern;
+/**Zadanie domowe ze zjazdu 3.
+     * Napisać funkcję liczZnakiSlowa, która zlicza:
+     * liczbę znaków w pliku,
+     * liczbę białych znaków w pliku (białe znaki to spacja, tabulator, znacznik końca wiersza),
+     * liczbę słów w pliku.
+     *
+     * Wynikiem funkcji jest tablica złożona z 3 liczb całkowitych po jednej dla wymienionych podpunktów.
+  Wykonał Sławomir Kamiński
+  GitHub user: Orzelzone.
+ */
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException  {
+    public static void main(String[] args)throws FileNotFoundException {
 	// write your code here
         String file = "test.txt";
-        /*Scanner scanner = new Scanner(new File(file));
-        while (scanner.hasNextLine()) {
-            System.out.println(scanner.nextLine());
-        }
-        scanner.close();*/
+         int znakiIlosc[] = new int[3];
+        znakiIlosc[0] = iloscZnak(file);
+        znakiIlosc[1] = iloscBialych(file);
+        znakiIlosc[2] = iloscSlow(file);
         System.out.println("W pliku '" +file+"' jest:");
-        System.out.println("Znaki drukowalne:       " + iloscZnak(file));
-        System.out.println("Znaki Białe:            " + iloscBialych(file));
-        System.out.println("Słowa:                  " + iloscSlow(file));
+        System.out.println("Znaki drukowalne:       " + znakiIlosc[0]);
+        System.out.println("Znaki Białe:            " + znakiIlosc[1]);
+        System.out.println("Słowa:                  " + znakiIlosc[2]);
     }
+    // metoda zwraca  ilosc słów
     public static int iloscSlow(String plik)throws FileNotFoundException {
         Scanner slowa = new Scanner(new File(plik));
         int slowo = 0;
@@ -30,7 +39,7 @@ public class Main {
         slowa.close();
         return slowo;
     }
-
+    // metoda zwraca ilosc znakow drukowalnych
     public static int iloscZnak(String plik)throws FileNotFoundException {
         Scanner znaki = new Scanner(new File(plik)).useDelimiter("[^\\s]");      //[^ \\t\\n\\x0B\\f\\r]");
         int znak = 0;
@@ -41,7 +50,7 @@ public class Main {
         znaki.close();
         return znak;
     }
-
+    //metoda zwraca ilość znaków białych
     public static int iloscBialych(String plik)throws FileNotFoundException {
         Scanner bialeZnaki = new Scanner(new File(plik)).useDelimiter("[ \\t\\n\\x0B\\f\\r]");
         int bialy = 0;
